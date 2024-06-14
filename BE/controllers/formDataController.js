3d1ea2f8df8dd5acedc52d7b7d4496070376ed42
TuQi00 <tuqiqi3007@gmail.com>
2024-06-14 14:01:24 +0700

    v3

--- BE/controllers/formDataController.js
+++ BE/controllers/formDataController.js
@@ -1,4 +1,4 @@
-const { FormData } = require('../dbconfigs/models');
+const { FormData, Booking } = require('../dbconfigs/models');
 
 const getFormData = async (req, res) => {
     try {
@@ -10,14 +10,14 @@ const getFormData = async (req, res) => {
     }
 };
 
-module.exports = getFormData;
+const getAllBookings = async (req, res) => {
+    try {
+        const bookings = await Booking.find();
+        res.json(bookings);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching bookings', error });
+    }
+};
 
-// db.formdatas.insertOne({
-//     services: [
-//         { name: 'Manicure', description: 'Basic manicure', price: 20, duration: 30 },
-//         { name: 'Pedicure', description: 'Basic pedicure', price: 25, duration: 40 },
-//         { name: 'Full Set', description: 'Full set of nails', price: 50, duration: 60 },
-//         { name: 'Fill', description: 'Nail fill', price: 30, duration: 45 }
-//     ],
-//     availableTimes: ['09:00', '10:00', '11:00', '13:00', '14:00', '15:00']
-// });
+module.exports = { getFormData, getAllBookings };
+
--- BE/routes/formDataRoutes.js
+++ BE/routes/formDataRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const getFormData = require('../controllers/formDataController');
+const {getFormData, getAllBookings} = require('../controllers/formDataController');
 const router = express.Router();
 
 router.get('/form-data', getFormData);
+router.get('/all-bookings', getAllBookings);
 
 module.exports = router;
